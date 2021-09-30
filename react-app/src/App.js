7275d3afd8ab46531745daaa5a6349b78330f7ea
g-veronika <g.veronika2021@gmail.com>
2021-09-30 18:01:14 +0200

    initial commit

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus delectus laboriosam eius est, doloribus laudantium quae accusamus laborum excepturi, a, quia voluptatum ipsum dolore deserunt maiores praesentium rem ut. Nam.</h1>
+    </div>
+    );
+}
+
+export default App;
